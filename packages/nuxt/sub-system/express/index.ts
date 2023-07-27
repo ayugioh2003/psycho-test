e27e4c6ba3340b007d0952ecea88302968579ef2
John <ayugioh2003@gmail.com>
2023-07-27 18:32:05 +0800

    chore: add cors

--- packages/nuxt/sub-system/express/index.ts
+++ packages/nuxt/sub-system/express/index.ts
@@ -1,6 +1,7 @@
 // https://stackoverflow.com/questions/62287709/environment-variable-with-dotenv-and-typescript
 import 'dotenv/config'
 import express from 'express'
+import cors from 'cors'
 
 import { connectDB } from './db'
 import router from './router'
@@ -11,6 +12,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
 app.use('/', router)
 
